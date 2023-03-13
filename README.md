c76cd183263b2e1cec06b14b57f28c789a9deb82
gzaprzala <zaprzala.grzegorz@gmail.com>
2023-03-13 13:44:17 +0100

    Added project structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# zaprzala-site
---
+++ src/views/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Grzegorz Zaprzała</h1>
+    </div>
+  )
+}
+
+export default App
